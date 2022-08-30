df4e5e24d35f084b0c46c2e96302f8d324f2d979
Ramon-NB <ramonster81@googlemail.com>
2022-08-30 21:04:33 +0200

    ausgabe korrigiert

--- main.js
+++ main.js
@@ -3,11 +3,11 @@
 
 let zahl;
 
-const zufallsZahl = (Math.floor(Math.random() * 101)).toFixed(0);
+const zufallsZahl = (Math.random() * 101).toFixed(0);
 
 console.log("Zufallszahl:", zufallsZahl);
 
-zahl = prompt("Geben Sie eine Zahl zwischen 0 und 100 ein");
+zahl = prompt("Versuch: 1 Geben Sie eine Zahl zwischen 0 und 100 ein");
 console.log(zahl);
 
 for (let i=1; i <= 10; i++) {
@@ -15,12 +15,12 @@ for (let i=1; i <= 10; i++) {
     alert("du hast "+ i+ " Versuche gebraucht");
     break;
   } else if (zahl < zufallsZahl) {
-    zahl = prompt("Versuch: " + i + " Die gesuchte Zahl ist größer");
+    zahl = prompt("Versuch: " + (i+1) + " Die gesuchte Zahl ist größer");
     console.log(zahl);
   } else if (i >= 11) {
-    alert("Versuch: " + i + " Du hast keine versuche mehr übrig");
+    alert("Versuch: " + (i+1) + " Du hast keine versuche mehr übrig");
   } else {
-    zahl = prompt("Versuch: " + i + " Die gesuchte Zahl ist kleiner");
+    zahl = prompt("Versuch: " + (i+1) + " Die gesuchte Zahl ist kleiner");
     console.log(zahl);
   }
 }
